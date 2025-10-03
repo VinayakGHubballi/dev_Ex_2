2b6f527ff2ac35bfda6d0646b9dc426c9ce2bd99
VinayakGHubballi <146335527+VinayakGHubballi@users.noreply.github.com>
2025-10-03 20:41:45 +0530

    Create salary_bonus.py

---
+++ salary_bonus.py
@@ -0,0 +1,9 @@
+# salary_bonus.py
+
+salary = float(input("Enter your salary: "))
+
+bonus = salary * 0.10
+total_salary = salary + bonus
+
+print("Original Salary:", salary)
+print("Bonus Added:", total_salary)
